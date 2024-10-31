6b49a5ec7768cfb44ac115926d18ae22f109b2f5
lredmill <langley.redmill@gmail.com>
2024-10-31 15:10:48 -0500

    Add files via upload
    instructions 
    20.11 Project 3: A Wavefront Path Finder v4.1
    
    Project 3: A Wavefront Path Finder
    
    (The project details are finished -- I have not completed the scoring/testing procedures.)
    
    ------------------------------NOTE:----------------------------------------------------------
    
    Main will call your function:
    
    distance = wavefront(filename,startrow,startcol,endrow,endcol,ouput);
    Return the distance and write the solution map to output. All other printing should go to std::cout -- only write the map to output (the checker will read this map and anything else will confuse it)
    
    But you can write to std::cout all you want.
    
    ------------------------------END NOTE----------------------------------------------------------
    
    You will read in a text file as a map Here is an example:
    
    ####################
    #.........##.......#
    #...##.....##......#
    #....##.....#......#
    #.....##....#..#...#
    #..#...##......#...#
    #..#....##.....#...#
    #..##....#.....#...#
    #...#....###...#...#
    #...##.....#...#...#
    #..............#...#
    ####################
    In the map file the pound character (#) will be used to represent an obstacle/wall. And a period(.) will be used to represent an open square.
    
    From any open square you can move to an adjacent square (no diagonal).
    
    Given a starting square(row,col) and an ending/target square(row,col) you will find the shortest path from the start to the target or report that no path exists.
    
    You will also print one path (the shortest path may not be unique) using a single quote(') to retrace the steps. eg:
    
    ####################
    #.''''''..##.......#
    #.'.##.'''.##......#
    #.'..##..''.#'''''.#
    #.@...##..''#'.#.'.#
    #..#...##..'''.#.'.#
    #..#....##.....#.'.#
    #..##....#.....#.'.#
    #...#....###...#.'.#
    #...##.....#...#.$.#
    #..............#...#
    ####################
    
    Shortest Path: 30
    There is simple well-known algorithm (wavefront) to follow that uses a queue. The algorithm works as follows:
    
    Initialize a queue with the starting square recording the distance as zero(0).
    
    Repeat until the queue is empty or the target has been reached:
    
    Dequeue a square and recover the distance of that square (distance matrix).
    Enqueue unvisited adjacent open squares with distance+1, this will be the minimum distance to the square
    Once the target has been found one can retrace the steps back to the start.
    
    From the target find any adjacent square that has distance-1 and repeat until you are at distance zero (the start cell)
    You have to work out some bookkeeping. You will have an array of strings to represent a map. A matrix of integers can be used to track visited squares and the distances. You might initialize all cells of this matrix with -1 as unvisited and then as you enqueue the cell you can update the value with a distance.
    
    When you find the target this will matrix allow you to find the path back to the starting cell. Just follow the bread crumbs.

--- main.cpp
+++ main.cpp
@@ -1,67 +1,41 @@
+//
+//  main.cpp
+//
+
+/*
+You will not upload this file as part of your project. 
+This program will read the filename, start and end positions
+then call your function with a stringstream for you to write to.
+On return it will print the contents of the string stream and 
+the distance returned by your function.
+*/
+
+#include <fstream>
+#include <sstream>
 #include <iostream>
-
-#include "zipcode.h"
-
-using namespace std;
-
-void print(string s, ZipCode codes[], int len) {
-    cout << s << endl;
-    for (int i = 0; i < len; i++) {
-        cout << "\t";
-        codes[i].print();
-        cout << endl;
-    }
-    cout << endl;
-}
-
-void bubbleSortInPlace(ZipCode codes[], int len, ZipCode::CompareType t) {
-    for(int j = 1; j < len; j++) {
-        bool madeChange = false;
-        for (int i = 0; i < len - j; i++) {
-            if (codes[i + 1].compare(codes[i], t)) {
-                swap(codes[i], codes[i + 1]);
-                madeChange = true;
-            }
-        }
-        if (!madeChange) break;
-    }
-}
-
-int main() {
-    ZipCode codes[25] = {
-        ZipCode(10001, "New York", "NY", 41, -74),
-        ZipCode(90001, "Los Angeles", "CA", 34, -118),
-        ZipCode(77001, "Houston", "TX", 30, -95),
-        ZipCode(60601, "Chicago", "IL", 42, -88),
-        ZipCode(78201, "San Antonio", "TX", 29, -98),
-        ZipCode(94101, "San Francisco", "CA", 38, -122),
-        ZipCode(19101, "Philadelphia", "PA", 40, -75),
-        ZipCode(85001, "Phoenix", "AZ", 33, -112),
-        ZipCode(95101, "San Jose", "CA", 37, -122),
-        ZipCode(48201, "Detroit", "MI", 42, -83),
-        ZipCode(43201, "Columbus", "OH", 40, -83),
-        ZipCode(46201, "Indianapolis", "IN", 40, -86),
-        ZipCode(02201, "Boston", "MA", 42, -71),
-        ZipCode(75201, "Dallas", "TX", 33, -97),
-        ZipCode(64101, "Kansas City", "MO", 39, -95),
-        ZipCode(73301, "Austin", "TX", 30, -98),
-        ZipCode(80201, "Denver", "CO", 40, -105),
-        ZipCode(92101, "San Diego", "CA", 33, -117),
-        ZipCode(73101, "Oklahoma City", "OK", 35, -97),
-        ZipCode(55401, "Minneapolis", "MN", 45, -93),
-        ZipCode(20001, "Washington", "DC", 39, -77),
-        ZipCode(30301, "Atlanta", "GA", 34, -84),
-        ZipCode(31901, "Columbus", "GA", 32, -85),
-        ZipCode(66101, "Kansas City", "KS", 39, -95),
-        ZipCode(35401, "Tuscaloosa", "AL", 33, -88)
-    };
-    int len = 25;
-
-    print("Zip Codes (unsorted)", codes, len);
-
-    // test your compare method here by changing the CompareType
-    bubbleSortInPlace(codes, len, ZipCode::CompareType::LessZip);
-    print("Zip Codes", codes, len);
-
+#include "wavefront.h"
+int main(int argc, const char * argv[]) {
+    std::string filename;
+    std::cout<<"filename: ";
+    std::cin >> filename;
+    
+    int srow, scol;
+    std::cout << "start row: ";
+    std::cin >> srow;
+    std::cout<<"start col: ";
+    std::cin >>scol;
+    
+    int xrow, xcol;
+    std::cout << "goal row: ";
+    std::cin >> xrow;
+    std::cout<<"goal col: ";
+    std::cin >> xcol;
+    
+    std::stringstream ss;
+    int distance;
+    distance = wavefront(filename,srow,scol,xrow,xcol,ss);
+    std::cout << ss.str() << std::endl;
+    std::cout << "Distance = " << distance << std::endl;
+    
     return 0;
 }
